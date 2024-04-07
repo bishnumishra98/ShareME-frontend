cf7961c69fb38dae5af49ec4279940abeb75fecd
Bishnu Mishra <mishravishnu369@gmail.com>
2024-04-07 14:10:28 +0530

    nothing

--- index.js
+++ index.js
@@ -111,7 +111,6 @@ const uploadFile = () => {
 
     xhr.open("POST", uploadURL);   // The open method initializes the request. It specifies the HTTP method (in this case, “POST”) and the URL (uploadURL) to which the request will be sent.
     xhr.send(formData);   // The send method sends the prepared formData (which includes the selected file) to the specified URL via an HTTP POST request.
-
 }
 
 // Functionality to calculate upload percentage of file at every some fraction of seconds
